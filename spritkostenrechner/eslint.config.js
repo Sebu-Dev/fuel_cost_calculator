2549c45eecaff2746220aae24aa9ce27476f4160
Sebu-Dev <fsebulke@gmail.com>
2024-12-14 23:05:12 +0100

    first commit

---
+++ spritkostenrechner/eslint.config.js
@@ -0,0 +1,38 @@
+import jsPlugin from '@eslint/js';
+import tsEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import reactPlugin from 'eslint-plugin-react';
+import globals from 'globals';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    },
+    plugins: {
+      react: reactPlugin,
+      '@typescript-eslint': tsEslint
+    },
+    rules: {
+      ...jsPlugin.configs.recommended.rules,
+      ...reactPlugin.configs.recommended.rules,
+      ...tsEslint.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off'
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    }
+  }
+];
