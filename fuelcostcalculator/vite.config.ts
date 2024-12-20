3c44b253f74235496cdb0f3e3a731764e366b9c2
Florian_Sebulke <fsebulke@gmail.com>
2024-12-20 11:26:50 +0100

    fix

--- fuelcostcalculator/vite.config.ts
+++ fuelcostcalculator/vite.config.ts
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/<fuel_cost_calculator>/'
+  base: '/fuel_cost_calculator/'
 });
